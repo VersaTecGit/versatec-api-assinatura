5ff796c09db8df398f80eef963bc7b6dc4d255cd
Fernando Maia <fernando.maia@versatecnologia.com.br>
2025-09-19 20:04:17 +0000

    Merged in VSA-8 (pull request #10)
    VSA-8
    
    * feat(VSA-8): Assinatura de xml e validacao de xml assinado.
    
    * feat(VSA-8): Liberando endpoint para documentacao e melhorando tratativas das exceptions.
    
    * VSA-8: Adicionando documentacao do response da funcao de assinar xml
    
    * VSA-8: Padronização na documentação de API e habilitar rota do swagger para acesso sem senha

--- src/main/java/com/versatec/config/SecurityConfig.java
+++ src/main/java/com/versatec/config/SecurityConfig.java
@@ -13,7 +13,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests ->
-                        requests.requestMatchers("/api/v1/qr-code").permitAll()
+                        requests.requestMatchers(
+                                "/api/v1/qr-code",
+                                "/swagger-ui/**",
+                                "/swagger/**",
+                                "/v1/api-docs/**"
+                        ).permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
--- src/main/java/com/versatec/controllers/SignerController.java
+++ src/main/java/com/versatec/controllers/SignerController.java
@@ -10,7 +10,7 @@ import com.versatec.services.SignatureValidationService;
 import com.versatec.utils.FileUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.media.Content;
+
 import org.demoiselle.signer.core.exception.CertificateValidatorException;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -49,8 +49,7 @@ public class SignerController {
             SignatureFileService signatureFileService,
             SignatureValidationService signatureValidationService,
             FileUtils fileUtils,
-            ObjectMapper objectMapper
-    ) {
+            ObjectMapper objectMapper) {
         this.signatureService = signatureService;
         this.signatureFileService = signatureFileService;
         this.signatureValidationService = signatureValidationService;
@@ -59,40 +58,24 @@ public class SignerController {
     }
 
     @PostMapping(path = "/sign", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Operation(
-            summary = "Assinar Documento",
-            description = "Assina um documento com assinador <b>CADES</b>, e certificado <b>A1</b>. <br/>" +
-                    "Caso seja enviada a <b>URL</b> onde o documento irá ser hospedado, inclui o <b>QR CODE</b>. <br/>" +
-                    "<b>PageIndex</b>, <b>X</b>, e <b>Y</b>, são parâmetros opcionais para customização da posição da assinatura visual. <br/><br/>" +
-                    "<b>PageIndex</b>: Começa de 0 e vai até o numero de páginas do documento -1. Para escolher automaticamente a " +
-                    "última página pode se enviar -1.<br/>" +
-                    "<b>X</b>: Margem a saltar do lado esquerdo da página. Valor padrão de assinatura sem QR: (Paisagem)356. (Retrato)233. <br/>" +
-                    "<b>Y</b>: Margem a saltar do lado inferior da página. Valor padrão de assinatura sem QR: 45. <br/>" +
-                    "<b>AllPages</b>(opcional): Se for <b>true</b>, a assinatura será aplicada em todas as páginas do documento. <br/>",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Documento assinado com sucesso",
-                            content = @Content(
-                                    mediaType = "application/pdf"
-                            )
-                    ),
-                    @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = String[].class)
-                            )
-                    ),
-                    @ApiResponse(responseCode = "401", description = "Senha do certificado é inválida",
-                            content = @Content(
-                                    mediaType = "text/plain",
-                                    schema = @Schema(implementation = String.class)
-                            )
-                    ),
-                    @ApiResponse(responseCode = "403", description = "Certificado inválido",
-                            content = @Content(
-                                    mediaType = "text/plain",
-                                    schema = @Schema(implementation = String.class)
-                            )
-                    ),
+    @Operation(summary = "Assinar Documento", description = "Assina um documento com assinador <b>CADES</b>, e certificado <b>A1</b>. <br/>"
+            +
+            "Caso seja enviada a <b>URL</b> onde o documento irá ser hospedado, inclui o <b>QR CODE</b>. <br/>"
+            +
+            "<b>PageIndex</b>, <b>X</b>, e <b>Y</b>, são parâmetros opcionais para customização da posição da assinatura visual. <br/><br/>"
+            +
+            "<b>PageIndex</b>: Começa de 0 e vai até o numero de páginas do documento -1. Para escolher automaticamente a "
+            +
+            "última página pode se enviar -1.<br/>" +
+            "<b>X</b>: Margem a saltar do lado esquerdo da página. Valor padrão de assinatura sem QR: (Paisagem)356. (Retrato)233. <br/>"
+            +
+            "<b>Y</b>: Margem a saltar do lado inferior da página. Valor padrão de assinatura sem QR: 45. <br/>"
+            +
+            "<b>AllPages</b>(opcional): Se for <b>true</b>, a assinatura será aplicada em todas as páginas do documento. <br/>", responses = {
+                    @ApiResponse(responseCode = "200", description = "Documento assinado com sucesso", content = @Content(mediaType = "application/pdf")),
+                    @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String[].class))),
+                    @ApiResponse(responseCode = "401", description = "Senha do certificado é inválida", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))),
+                    @ApiResponse(responseCode = "403", description = "Certificado inválido", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))),
             })
     public ResponseEntity<?> sign(
             @RequestParam(required = false) @NotNull MultipartFile file,
@@ -114,12 +97,14 @@ public class SignerController {
             var customCertificate = new CustomCertificate(certificatePath, password);
 
             byte[] signedDocument = this.signatureService.signDocument(filePath, customCertificate);
-            outputPath = this.signatureFileService.createPDF(filePath, signedDocument, customCertificate, visualSignatureConfig, url);
+            outputPath = this.signatureFileService.createPDF(filePath, signedDocument, customCertificate,
+                    visualSignatureConfig, url);
 
             var signedPdfData = Files.readAllBytes(outputPath);
 
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=signed_" + file.getOriginalFilename());
+            headers.add(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=signed_" + file.getOriginalFilename());
 
             return ResponseEntity.ok()
                     .headers(headers)
@@ -140,30 +125,17 @@ public class SignerController {
     }
 
     @PostMapping(path = "/validate-signature", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Operation(
-            summary = "Validar assinatura",
-            description = "Valida se todas as assinaturas de um documento são válidas",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "A assinatura é valida",
-                            content = @Content(
-                                    mediaType = "text/plain",
-                                    schema = @Schema(implementation = String.class)
-                            )
-                    ),
-                    @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido",
-                            content = @Content(
-                                    mediaType = "text/plain",
-                                    schema = @Schema(implementation = String[].class)
-                            )
-                    )
-            })
+    @Operation(summary = "Validar assinatura", description = "Valida se todas as assinaturas de um documento são válidas", responses = {
+            @ApiResponse(responseCode = "200", description = "A assinatura é valida", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String[].class)))
+    })
     public ResponseEntity<String> validateSignature(
-            @RequestParam(required = false) @NotNull MultipartFile file
-    ) throws IOException {
+            @RequestParam(required = false) @NotNull MultipartFile file) throws IOException {
         var filePath = this.fileUtils.uploadFile(file, FileLocationEnum.UPLOAD);
 
         try {
-            List<SignatureInformations> results = this.signatureValidationService.validateAllSignatures(filePath);
+            List<SignatureInformations> results = this.signatureValidationService
+                    .validateAllSignatures(filePath);
 
             if (!results.isEmpty()) {
                 return ResponseEntity.ok("Valid document");
@@ -178,33 +150,14 @@ public class SignerController {
     }
 
     @PostMapping(path = "/validate-certificate", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @Operation(
-            summary = "Validar certificado",
-            description = "Valida se um certificado é valido",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "O certificado é valido",
-                            content = @Content(
-                                    mediaType = "text/plain",
-                                    schema = @Schema(implementation = String.class)
-                            )
-                    ),
-                    @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = String[].class)
-                            )
-                    ),
-                    @ApiResponse(responseCode = "401", description = "A senha ou certificado é inválido",
-                            content = @Content(
-                                    mediaType = "text/plain",
-                                    schema = @Schema(implementation = String.class)
-                            )
-                    )
-            })
+    @Operation(summary = "Validar certificado", description = "Valida se um certificado é valido", responses = {
+            @ApiResponse(responseCode = "200", description = "O certificado é valido", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String[].class))),
+            @ApiResponse(responseCode = "401", description = "A senha ou certificado é inválido", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class)))
+    })
     public ResponseEntity<String> validateCertificate(
             @RequestParam(required = false) @NotNull MultipartFile certificate,
-            @RequestParam(required = false) @NotNull @NotEmpty String password
-    ) throws IOException {
+            @RequestParam(required = false) @NotNull @NotEmpty String password) throws IOException {
         var certificatePath = this.fileUtils.uploadFile(certificate, FileLocationEnum.UPLOAD);
 
         try {
@@ -221,36 +174,22 @@ public class SignerController {
     }
 
     @GetMapping("/qr-code")
-    @Operation(
-            summary = "Url na qual os Qr Codes apontam",
-            description = "Caso lido pela câmera do celular, redireciona o usuário para o site <b>validar.iti.gov.br</b>. <br/>" +
-                    "Caso lido pelo validador do site, retorna um json com a URL do pdf, para o site realizar o download do arquivo. <br/><br/>" +
-                    "<b>_format</b> e <b>_secretCode</b> são parâmetros criados para utilização do validador do governo",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Json com url do documento",
-                            content = @Content(
-                                    mediaType = "application/json"
-                            )
-                    ),
-                    @ApiResponse(responseCode = "303", description = "Redirecionamento para validar.iti.gov.br",
-                            content = @Content(
-                                    mediaType = ""
-                            )
-                    ),
-                    @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = String[].class)
-                            )
-                    ),
+    @Operation(summary = "Url na qual os Qr Codes apontam", description = "Caso lido pela câmera do celular, redireciona o usuário para o site <b>validar.iti.gov.br</b>. <br/>"
+            +
+            "Caso lido pelo validador do site, retorna um json com a URL do pdf, para o site realizar o download do arquivo. <br/><br/>"
+            +
+            "<b>_format</b> e <b>_secretCode</b> são parâmetros criados para utilização do validador do governo", responses = {
+                    @ApiResponse(responseCode = "200", description = "Json com url do documento", content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "303", description = "Redirecionamento para validar.iti.gov.br", content = @Content(mediaType = "")),
+                    @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String[].class))),
             })
     public ResponseEntity<String> qrCode(
             @RequestParam(value = "_format", required = false) String format,
             @RequestParam(value = "_secretCode", required = false) String secretCode,
-            @RequestParam(value = "url", required = false) @NotNull @NotEmpty String returnUrl
-    ) throws URISyntaxException, JsonProcessingException {
+            @RequestParam(value = "url", required = false) @NotNull @NotEmpty String returnUrl)
+            throws URISyntaxException, JsonProcessingException {
         if (Objects.equals(format, "application/validador-iti json")) {
-            var result = new Object(){
+            var result = new Object() {
                 final String url = returnUrl;
             };
             String json = this.objectMapper.writeValueAsString(result);
@@ -262,4 +201,51 @@ public class SignerController {
             return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
         }
     }
+
+    @PostMapping(path = "/sign-xml", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Assina um arquivo XML", description = "Realiza a assinatura digital de um documento xml, utilizando um certificado digital.",  responses = {
+                    @ApiResponse(responseCode = "200", description = "Documento assinado com sucesso", content = @Content(mediaType = "application/pdf")),
+                    @ApiResponse(responseCode = "401", description = "Bad Request - Algum dado enviado é invalido, formato do arquivo ou certificado", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String[].class)))
+    })
+    public ResponseEntity<?> signXml(
+            @RequestParam @NotNull MultipartFile file,
+            @RequestParam @NotNull MultipartFile certificate,
+            @RequestParam @NotNull String password) throws IOException {
+
+        var filePath = this.fileUtils.uploadFile(file, FileLocationEnum.UPLOAD);
+        var certificatePath = this.fileUtils.uploadFile(certificate, FileLocationEnum.UPLOAD);
+
+        try {
+            var customCertificate = new CustomCertificate(certificatePath, password);
+            customCertificate.checkValidity();
+
+            var signedDocument = this.signatureService.signXmlDocument(filePath, customCertificate);
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getOriginalFilename())
+                    .body(signedDocument);
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        } finally {
+            this.fileUtils.removeFile(certificatePath);
+        }
+    }
+
+    @PostMapping(path = "/validate-xml-signature", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Validar assinatura de xml", description = "Valida se um documento xml está assinado", responses = {
+            @ApiResponse(responseCode = "200", description = "A assinatura é valida", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Algum dado enviado é invalido", content = @Content(mediaType = "text/plain", schema = @Schema(implementation = String[].class)))
+    })
+    public ResponseEntity<?> validateXmlSignature(@RequestParam("file") MultipartFile file) {
+        try {
+            var filePath = this.fileUtils.uploadFile(file, FileLocationEnum.UPLOAD);
+            var isValid = this.signatureValidationService.validateXmlSignature(filePath);
+
+            var responseMessage = isValid ? "Sined Document" : "Unsigned document";
+            return ResponseEntity.ok(responseMessage);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File upload failed: " + e.getMessage());
+        }
+    }
+
 }
